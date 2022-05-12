da9288bfbabf3c2d8694ca9bcbcbda91c13e802c
n-hidaka <85332716+n-hidaka@users.noreply.github.com>
2022-05-12 20:38:19 +0900

    fix browser console error, @emotion/core -> @emotion/react
    - browser console error:
      The `@emotion/core` package has been renamed to `@emotion/react`.

--- src/conf/main.tsx
+++ src/conf/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { Global } from "@emotion/core";
+import { Global } from "@emotion/react";
 import debug from "debug";
 import packageInfo from "../../package.json";
 import { globalStyle } from "../shared/global-style";
